7429a0b0fad32953cab2d9d0745d9e3c814a342c
johannes karoff <johannes@karoff.net>
2020-03-31 10:45:00 +0200

    add more options to flatpickr emitterbuilder

--- flatpickr/src/main/scala/outwatch/libs/flatpickr/Flatpickr.scala
+++ flatpickr/src/main/scala/outwatch/libs/flatpickr/Flatpickr.scala
@@ -8,19 +8,26 @@ import outwatch.dsl._
 import colibri._
 
 trait Flatpickr {
-  def flatpickr(placeholder: String, withTime: Boolean, initialDate: Option[js.Date], isInline: Boolean = false, openTrigger: Observable[Boolean] = Observable.empty): EmitterBuilder[js.Date, VDomModifier] = EmitterBuilder.ofModifier { sink =>
+  def flatpickr(placeholder: String, withTime: Boolean = false, initialDate: Option[js.Date] = None, format: Option[String] = None, isInline: Boolean = false, openTrigger: Observable[Boolean] = Observable.empty): EmitterBuilder[js.Date, VDomModifier] =
+    flatpickrWithOptions(placeholder, new facade.FlatpickrOptions {
+      enableTime = withTime
+      altInput = true
+      inline = isInline
+      defaultDate = initialDate.orUndefined
+      dateFormat = format.orUndefined
+    }, openTrigger)
+
+  def flatpickrWithOptions(placeholder: String, options: facade.FlatpickrOptions, openTrigger: Observable[Boolean] = Observable.empty): EmitterBuilder[js.Date, VDomModifier] = EmitterBuilder.ofModifier { sink =>
     input(
       dsl.placeholder := placeholder,
       managedElement { elem =>
-        val instance = facade.Flatpickr(elem, new facade.FlatpickrOptions {
-          enableTime = withTime
-          altInput = true
-          inline = isInline
-          defaultDate = initialDate.orUndefined
-          onClose = { (dateArr, _, _) =>
-            if (dateArr.nonEmpty) sink.onNext(dateArr.head)
-          }: js.Function3[js.Array[js.Date], String, js.Any, Unit]
-        })
+        val oldOnClose = options.onClose
+        options.onClose = { (dateArr, a, b) =>
+          oldOnClose.foreach(_(dateArr, a, b))
+          if (dateArr.nonEmpty) sink.onNext(dateArr.head)
+        }: js.Function3[js.Array[js.Date], String, js.Any, Unit]
+
+        val instance = facade.Flatpickr(elem, options)
 
         Cancelable.composite(
           Cancelable(instance.destroy),
